a3d2f194d9dc4bcb366a0f4be0dd775255f209b6
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-01-15 22:18:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 // write your code below!
 
 var name = "Mera"
-var height = "63"
+var height = 63
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height) + 1)
+console.log(height +1)
+  
+  //parseInt(height) + 1)
 
